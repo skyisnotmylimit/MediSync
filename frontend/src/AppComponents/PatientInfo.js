cc4ab66afc1e20ded482b5f4cdc67a1ee1040b96
ShubhamGupta9453 <shubhamgupta9454666551@gmail.com>
2023-09-26 11:30:13 +0530

    folder relocated

--- frontend/src/AppComponents/PatientInfo.js
+++ frontend/src/AppComponents/PatientInfo.js
@@ -70,7 +70,7 @@ const PatientInfo = () => {
     userData = inputValue;
     // POST this userData
     console.log(userData);
-    const url = "https://https://medi-sync1.vercel.app/patient-dashboard";
+    const url = "http://localhost:3000/patient-dashboard";
     try {
       const response = await axios.post(url, userData)
       console.log("Response:", response.data);
